6cf6ff3f70eaf6ced729373e3ef21396a3db54df
Perry Stephenson <perry.stephenson@gmail.com>
2016-10-08 16:23:52 +1100

    New file install_and_load_packages.R

---
+++ Install/install_and_load_packages.R
@@ -0,0 +1,40 @@
+# This script checks whether a package can be loaded, and either:
+# 1. loads it, or
+# 2. installs it and then loads it
+# Failure to load will result in an error.
+
+# text2vec
+if(!require(text2vec)) {
+  install.packages("text2vec")
+  library(text2vec)
+}
+
+# tidytext
+if(!require(tidytext)) {
+  install.packages("tidytext")
+  library(tidytext)
+}
+
+# NLP
+if(!require(NLP)) {
+  install.packages("NLP")
+  library(NLP)
+}
+
+# coreNLP
+if(!require(coreNLP)) {
+  install.packages("coreNLP")
+  library(coreNLP)
+}
+
+# caret
+if(!require(caret)) {
+  install.packages("caret")
+  library(caret)
+}
+
+# stm
+if(!require(stm)) {
+  install.packages("stm")
+  library(stm)
+}
